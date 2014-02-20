1bb00843bf7cd4fa9f301f36000069bd2b4ba039
Mighty-Chewbacca <alexmalcolm93@live.co.uk>
2014-02-20 16:58:56 +0000

    TheHub- FirstIteration
    First iteration of the "hub", sprites change when "Clyde" is clicked,
    now need to implement a way to choose which building to upgrade

---
+++ IntegratedProject/Assets/Scripts/TheHub/ChangeSprites.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSprites : MonoBehaviour 
+{
+	public int currentSprite;
+	public Sprite level1, level2, level3;
+	private SpriteRenderer myRenderer;
+
+	void Start () 
+	{
+		myRenderer = gameObject.GetComponent<SpriteRenderer>();
+		currentSprite = 0;
+		myRenderer.sprite = level1;
+	}
+
+	void Update ()
+	{
+		if (currentSprite == 1) 
+		{
+			myRenderer.sprite = level2;
+		}
+		if (currentSprite == 2) 
+		{
+			myRenderer.sprite = level3;
+		}
+	}
+}
---
+++ IntegratedProject/Assets/Scripts/TheHub/ClydeChat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClydeChat : MonoBehaviour
+{
+	public ChangeSprites Building1, Building2, Building3;
+
+	void Start ()
+	{
+	}
+
+	void OnMouseDown()
+	{
+		Building1.currentSprite ++;
+		Building2.currentSprite ++;
+		Building3.currentSprite ++;
+	}
+
+	void Update () 
+	{
+
+	}
+}
