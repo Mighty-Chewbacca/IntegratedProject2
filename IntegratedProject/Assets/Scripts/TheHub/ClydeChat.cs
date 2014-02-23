22607afd1f0ec52e1199f5fc673e59a4a48eb17a
Mighty-Chewbacca <alexmalcolm93@live.co.uk>
2014-02-23 23:25:57 +0000

    Created "Buttons"
    updated the hub and created buttons to press, no code as of yet.
    will be implemented tonight

--- IntegratedProject/Assets/Scripts/TheHub/ClydeChat.cs
+++ IntegratedProject/Assets/Scripts/TheHub/ClydeChat.cs
@@ -4,16 +4,27 @@ using System.Collections;
 public class ClydeChat : MonoBehaviour
 {
 	public ChangeSprites Building1, Building2, Building3;
+    public GameObject chatBox, B1Button, B2Button, B3Button;
+    private bool canPress;
 
 	void Start ()
 	{
+        canPress = true;
 	}
 
 	void OnMouseDown()
 	{
-		Building1.currentSprite ++;
-		Building2.currentSprite ++;
-		Building3.currentSprite ++;
+        if (canPress == true)
+        {
+            canPress = false;
+            Instantiate(chatBox, new Vector2(-2, 4), Quaternion.identity);
+            Instantiate(B1Button, new Vector2(4, 2), Quaternion.identity);
+            Instantiate(B2Button, new Vector2(2, 2), Quaternion.identity);
+            Instantiate(B3Button, new Vector2(0, 2), Quaternion.identity);
+        }
+		//Building1.currentSprite ++;
+		//Building2.currentSprite ++;
+		//Building3.currentSprite ++;
 	}
 
 	void Update () 
