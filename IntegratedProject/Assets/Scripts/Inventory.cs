3b3b0934a979fb7144c32a5c9ef47cc241833958
Mighty-Chewbacca <amalco200@Lab-M627-Mac-16.local>
2014-03-07 12:27:36 +0000

    Added Pickups
    Pickups have been added, they increment an int in the players
    inventory, and then delete themselves.
    still need some work, then only increment the can int and not their own
    type.

---
+++ IntegratedProject/Assets/Scripts/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour 
+{
+
+	public int can, bottle, paper;
+
+	void Start () 
+	{
+
+	}
+
+	void Update () 
+	{
+		//something in here
+	}
+}
--- IntegratedProject/Assets/Scripts/PickupScript.cs
+++ IntegratedProject/Assets/Scripts/PickupScript.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class PickupScript : MonoBehaviour 
 {
     public Texture2D pickupSprite;
+	public Inventory playerInv;
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            print("coin got");
-
+            print("can got");
+			playerInv.can ++;
             //update gui
             //set coin to inactive
             gameObject.SetActive(false);
