0739e1d4f6d6887ca82ecd94479c223f07276233
Mighty-Chewbacca <amalco200@Lab-M627-Mac-16.local>
2014-03-25 11:59:12 +0000

    added adrians stuff
    ...

---
+++ IntegratedProject/Assets/Scripts/Enemies/EnemyShootScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShootScript : MonoBehaviour 
+{
+	private Vector3 rotation = new Vector3(-1, 0, 0);
+	public GameObject objBullet;
+	public bool damageCalled;
+	public int timeBetweenShooting; 
+
+	void Start ()
+	{
+		damageCalled = false;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (damageCalled == false) 
+		{
+			damageCalled = true;
+			StartCoroutine (DamageTimer ());
+		}
+	}
+
+	IEnumerator DamageTimer() 
+	{
+		Instantiate (objBullet, transform.position, Quaternion.LookRotation (rotation));
+		yield return new WaitForSeconds (timeBetweenShooting);
+		damageCalled = false;
+	}
+
+
+}
